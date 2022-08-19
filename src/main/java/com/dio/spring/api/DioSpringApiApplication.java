1624d7b306a0b517330980bc7a12309fc7b9ce7f
RafaelRenaud <renaudrafa@hotmail.com>
2022-08-19 19:14:16 -0300

    Criação do projeto

---
+++ src/main/java/com/dio/spring/api/DioSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/spring/api/DioSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.spring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

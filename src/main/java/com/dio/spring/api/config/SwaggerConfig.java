2d1e50408d1bf63da72054a346fa07535010b509
RafaelRenaud <renaudrafa@hotmail.com>
2022-08-20 17:59:45 -0300

    conf: Adicionando Módulo de Segunrança Spring Security

--- src/main/java/com/dio/spring/api/config/SwaggerConfig.java
+++ src/main/java/com/dio/spring/api/config/SwaggerConfig.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,7 +24,23 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.dio.spring.api"))
                 .build()
                 .pathMapping("/")
-                .apiInfo(metaData());
+                .apiInfo(metaData())
+                .securityContexts(Arrays.asList(actuatorSecurityContext()))
+                .securitySchemes(Arrays.asList(basicAuthScheme()));
+    }
+
+    private SecurityContext actuatorSecurityContext(){
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(basicAuthReference()))
+                .build();
+    }
+
+    private SecurityScheme basicAuthScheme(){
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference(){
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
     }
 
     private ApiInfo metaData() {
